ad05345f5ebd74aaacb8c2f6c7215929ded18a92
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-03-07 17:42:39 +0100

    array membriStaff

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let membriStaff = [
+
+    personaUno = {
+        nome: 'Angela Caroll',
+        lavoro: 'Chief Editor',
+        foto: '../img/angela-caroll-editor.jpg',
+    },
+
+    personaDue = {
+        nome: 'Walter Gordon',
+        lavoro: 'Office Manager',
+        foto: '../img/walter-gordon-office-manager.jpg',
+    },
+
+    personaTre = {
+        nome: 'Angela Lopez',
+        lavoro: 'Social Media Manager',
+        foto: '../img/angela-lopez-social-media-manager.jpg',
+    },
+
+    personaQuattro = {
+        nome: 'Scott Estrada',
+        lavoro: 'Developer',
+        foto: '../img/scott-estrada-developer.jpg',
+    },
+
+    personaCinque = {
+        nome: 'Barbara Ramos',
+        lavoro: 'Graphic Designer',
+        foto: '../img/barbara-ramos-graphic-designer.jpg',
+    }
+
+]

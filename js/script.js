e55bebe202bacd2a379888a6d1892c846be1d609
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-03-07 18:33:12 +0100

    fix append

--- js/script.js
+++ js/script.js
@@ -36,37 +36,39 @@ let membriStaff = [
 function creaCarta(immagine, nomePersona, posizione) {
 
     const divContainer = document.querySelector('.team-container');
-    console.log(divContainer);
 
     const divTeamCard = document.createElement('div');
     divTeamCard.classList.add('team-card');
     divContainer.append(divTeamCard);
-    console.log(divTeamCard);
 
     const divCardImage = document.createElement('div');
     divCardImage.classList.add('card-image');
     divCardImage.innerHTML = `<img src="${immagine}" alt="${nomePersona}" />`;
     divTeamCard.append(divCardImage);
-    console.log(divCardImage);
 
     const divCardText = document.createElement('div');
     divCardText.classList.add('card-text');
     divTeamCard.append(divCardText);
-    console.log(divCardText);
-
 
     const h3Nome = document.createElement('h3');
     h3Nome.innerHTML = `${nomePersona}`;
-    divTeamCard.append(h3Nome);
-    console.log(h3Nome);
+    divCardText.append(h3Nome);
 
     const pLavoro = document.createElement('p');
     pLavoro.innerHTML = `${posizione}`;
-    divTeamCard.append(pLavoro);
-    console.log(pLavoro);
+    divCardText.append(pLavoro);
 
 }
 
 for (let index = 0; index < 5; index++) {
     creaCarta(membriStaff[index].foto, membriStaff[index].nome, membriStaff[index].lavoro);
 }
+
+
+
+const inputName = document.getElementById('name').value;
+const inputRole = document.getElementById('role').value;
+const inputImage = document.getElementById('image').value;
+const btnAdd = document.getElementById('addMemberButton');
+
+btnAdd.addEventListener('click', creaCarta(inputImage, inputName, inputRole));
